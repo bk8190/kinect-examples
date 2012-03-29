7d81cae644e63b756627f9f94c17ffa91df1ad87
Bill Kulp <bk8190@gmail.com>
2012-03-29 18:10:42 -0400

    Will now transform point to frame specified by "global_frame"

--- image_cloud_sub/src/image_cloud_sub.cpp
+++ image_cloud_sub/src/image_cloud_sub.cpp
@@ -7,6 +7,8 @@
 #include <message_filters/subscriber.h>
 #include <message_filters/synchronizer.h>
 #include <message_filters/sync_policies/exact_time.h>
+#include <message_filters/sync_policies/approximate_time.h>
+#include <tf/transform_listener.h>
 
 #include <boost/format.hpp>
 
@@ -33,6 +35,8 @@ typedef pcl::PointCloud<pcl::PointXYZRGB> PointCloudXYZRGB;
 ros::Publisher             cloud_pub_;
 image_transport::Publisher image_pub_;
 string window_name_;
+tf::TransformListener* tfl_;
+string global_frame_ = "base_link";
 
 // A magical callback that combines an image, cam info, and point cloud
 void allCB(const sensor_msgs::ImageConstPtr& image_msg, 
@@ -64,10 +68,28 @@ void allCB(const sensor_msgs::ImageConstPtr& image_msg,
 	cv::circle(cv_ptr->image, cv::Point(col,row), 10, CV_RGB(255,0,0));
 	
 	// Get the corresponding 3D point
-	pcl::PointXYZRGB p = cloud.at(col, row);
+	pcl::PointXYZRGB pcl_pt = cloud.at(col, row);
+	geometry_msgs::Point geom_pt;
+	geom_pt.x = pcl_pt.x; geom_pt.y = pcl_pt.y; geom_pt.z = pcl_pt.z;
 	
-	ROS_INFO_STREAM(boost::format("Pixel (%d,%d) maps to 3D point (%.2f,%.2f,%.2f) in TF frame = \"%s\"")
-		%row %col %p.x %p.y %p.z %cloud_msg->header.frame_id);
+	// the TF package requires inputs to be in the form Stamped<sometype>
+	tf::Stamped<tf::Point> geom_pt_tf, temp_pt_tf;
+	pointMsgToTF(geom_pt, geom_pt_tf);
+	geom_pt_tf.frame_id_ = cloud_msg->header.frame_id;
+	geom_pt_tf.stamp_    = cloud_msg->header.stamp;
+	
+	try {
+		tfl_->transformPoint(global_frame_, geom_pt_tf, temp_pt_tf);
+	}
+	catch(tf::TransformException& ex) {
+		ROS_ERROR_STREAM(boost::format("Failed to transform point pose from \"%s\" to \"%s\" frame: %s")
+			%geom_pt_tf.frame_id_ %global_frame_ %ex.what());
+		return;
+	}
+	tf::pointTFToMsg(temp_pt_tf, geom_pt);
+	
+	ROS_INFO_STREAM(boost::format("Pixel (%d,%d) maps to 3D point (%.2f,%.2f,%.2f) in frame = \"%s\"")
+		%row %col %geom_pt.x %geom_pt.y %geom_pt.z %global_frame_);
 	
 	// Show the image.  The window does not update without the cvWaitKey.
 	cv::imshow(window_name_.c_str(), cv_ptr->image);
@@ -85,6 +107,9 @@ int main (int argc, char** argv)
 	ros::NodeHandle private_nh("~");
 	image_transport::ImageTransport it(nh);
 	
+	tf::TransformListener tfl;
+	tfl_ = &tfl;
+	
 	// Get some parameters (optional)
 	string mystringparam;
 	double mydoubleparam;
@@ -93,6 +118,9 @@ int main (int argc, char** argv)
 	ROS_INFO_STREAM("mystringparam = " << mystringparam);
 	ROS_INFO_STREAM("mydoubleparam = " << mydoubleparam);
 	
+	private_nh.param("global_frame" , global_frame_, string("this is a default value"));
+	ROS_INFO_STREAM("Using global frame \""<<global_frame_<<"\"");
+	
 	// Subscribe to an image, cloud, and camera info.
 	// Note the use of image_transport::SubscriberFilter and message_filters::Subscriber.  These allow for synchronization of the topics.
 	image_transport::SubscriberFilter                    image_sub   (it, "in_image", 1);
@@ -100,7 +128,7 @@ int main (int argc, char** argv)
 	message_filters::Subscriber<sensor_msgs::CameraInfo> cam_info_sub(nh, "in_cam_info", 1);
 	
 	// This sync policy will invoke a callback if it receives one of each message with matching timestamps
-	typedef message_filters::sync_policies::ExactTime
+	typedef message_filters::sync_policies::ApproximateTime
 	   <sensor_msgs::Image, sensor_msgs::PointCloud2, sensor_msgs::CameraInfo> MySyncPolicy;
 	
 	// Synchronize the three topics.  MySyncPolicy(10) tells it to maintain a buffer of 10 messages.
@@ -111,7 +139,7 @@ int main (int argc, char** argv)
 	sync.registerCallback( boost::bind(&allCB, _1, _2, _3) );
 	
 	// publishers for the image and point cloud
-  image_pub_ = it.advertise                  (ros::this_node::getName() + "/out_image", 1);
+  image_pub_ = it.advertise                          (ros::this_node::getName() + "/out_image", 1);
 	cloud_pub_ = nh.advertise<sensor_msgs::PointCloud2>(ros::this_node::getName() + "/out_cloud", 1);
 
 	window_name_ = "Image from Kinect";
